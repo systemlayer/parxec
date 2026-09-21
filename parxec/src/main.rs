cbaa20aa790f4de47d29cf2a0e73be2764da1f68
systemlayer <174369883+systemlayer@users.noreply.github.com>
2026-09-21 20:20:43 +0000

    parxec: implement input discovery

--- parxec/src/main.rs
+++ parxec/src/main.rs
@@ -1,10 +1,12 @@
 #[allow(dead_code)]
 mod cli;
+mod input;
 
 use clap::Parser;
 use cli::{AnalyzeArgs, Cli, Commands, HashArgs, RunArgs};
 
-fn hash(_args: HashArgs) -> anyhow::Result<()> {
+fn hash(args: HashArgs) -> anyhow::Result<()> {
+  input::discover_files(&args.input_dir, args.file_limit)?;
   anyhow::bail!("hash is not implemented yet")
 }
 
@@ -12,7 +14,8 @@ fn analyze(_args: AnalyzeArgs) -> anyhow::Result<()> {
   anyhow::bail!("analyze is not implemented yet")
 }
 
-fn run(_args: RunArgs) -> anyhow::Result<()> {
+fn run(args: RunArgs) -> anyhow::Result<()> {
+  input::discover_files(&args.input_dir, args.file_limit)?;
   anyhow::bail!("run is not implemented yet")
 }
 
